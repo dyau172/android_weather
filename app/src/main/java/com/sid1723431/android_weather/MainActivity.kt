274a9fe52dd4d12f468b4edc63db9aaece00d856
dyau172 <47676379+dyau172@users.noreply.github.com>
2021-10-20 22:29:49 +0100

    initial commit

---
+++ app/src/main/java/com/sid1723431/android_weather/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sid1723431.android_weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
