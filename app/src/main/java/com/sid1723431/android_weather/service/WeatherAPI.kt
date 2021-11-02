e31836bedd8c9a6bfcc594d0c069e66ffdf99453
dyau172 <47676379+dyau172@users.noreply.github.com>
2021-11-02 20:32:29 +0000

    created weather api

---
+++ app/src/main/java/com/sid1723431/android_weather/service/WeatherAPI.kt
@@ -0,0 +1,17 @@
+package com.sid1723431.android_weather.service
+
+import com.sid1723431.android_weather.model.WeatherModel
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherAPI {
+
+    //http://api.openweathermap.org/data/2.5/weather?q=bingöl&APPID=813e3a9b93bdb7cd510452b0e49af603
+
+    @GET("data/2.5/weather?&units=metric&APPID=813e3a9b93bdb7cd510452b0e49af603")
+    fun getData(
+        @Query("q") cityName: String
+    ): Single<WeatherModel>
+
+}
