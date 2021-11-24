451df89fe6008b0276e4b5061206f05f5e5d9b7e
Donna Yau <donna.yau@msn.com>
2021-11-24 22:10:54 +0000

    http to https

--- app/src/main/java/com/sid1723431/android_weather/service/WeatherAPI.kt
+++ app/src/main/java/com/sid1723431/android_weather/service/WeatherAPI.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface WeatherAPI {
 
-    //http://api.openweathermap.org/data/2.5/weather?q=bingöl&APPID=813e3a9b93bdb7cd510452b0e49af603
+    //https://api.openweathermap.org/data/2.5/weather?q=bingöl&APPID=813e3a9b93bdb7cd510452b0e49af603
 
     @GET("data/2.5/weather?&units=metric&APPID=813e3a9b93bdb7cd510452b0e49af603")
     fun getData(
--- app/src/main/java/com/sid1723431/android_weather/service/WeatherAPIService.kt
+++ app/src/main/java/com/sid1723431/android_weather/service/WeatherAPIService.kt
@@ -8,9 +8,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherAPIService {
 
-    //http://api.openweathermap.org/data/2.5/weather?q=bingol&APPID=813e3a9b93bdb7cd510452b0e49af603
+    //https://api.openweathermap.org/data/2.5/weather?q=bingol&APPID=813e3a9b93bdb7cd510452b0e49af603
 
-    private val BASE_URL = "http://api.openweathermap.org/"
+    private val BASE_URL = "https://api.openweathermap.org/"
 
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
